e20fadbf12f3ae135dbb57214d7eb6705c84cd20
UtsavGohel <utsavgohel2002@gmail.com>
2025-03-27 12:52:35 +0530

    refactor(footer): refactor footer component and links

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,24 +1,23 @@
-import { FaGithub, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className=" bg-gray-900 text-white flex flex-col items-center justify-center px-6 py-5">
-      <footer className="mt-10 py-6 text-center border-t border-gray-700 w-full">
-        <p className="text-gray-400">
-          &copy; {new Date().getFullYear()} Textify. All rights reserved.
-        </p>
-        <div className="flex justify-center space-x-4 mt-2">
-          <FaGithub
-            className="text-gray-400 hover:text-white cursor-pointer"
-            size={20}
-          />
-          <FaTwitter
-            className="text-gray-400 hover:text-white cursor-pointer"
-            size={20}
-          />
-        </div>
-      </footer>
-    </div>
+    <footer className="bg-gray-900 text-white py-6 flex flex-col items-center border-t border-gray-700 w-full">
+      <p className="text-gray-400 text-lg">
+        &copy; {new Date().getFullYear()} Textify. All rights reserved.
+      </p>
+      <div className="flex space-x-6 mt-3">
+        <Link href="/legal/terms" className="text-gray-400 hover:text-white">
+          Terms of Service
+        </Link>
+        <Link href="/legal/privacy" className="text-gray-400 hover:text-white">
+          Privacy Policy
+        </Link>
+        <Link href="/legal/contact" className="text-gray-400 hover:text-white">
+          Contact Us
+        </Link>
+      </div>
+    </footer>
   );
 };
 
